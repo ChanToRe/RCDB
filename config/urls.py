e7c7a6a76a247232c54bb41fe6e17e3517e51cfe
ChanHyeok <wnrnrxla@gmail.com>
2021-06-25 20:50:06 +0900

    New : main
    메인 페이지 기능 추가

--- config/urls.py
+++ config/urls.py
@@ -21,4 +21,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('notice/', include('notice.urls')),
     path('info/', include('info.urls')),
+    path('', include('main.urls')),
 ]
