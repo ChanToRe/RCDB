da4ecae223e9f201927f483f43631623e9b0c638
ChanHyeok <wnrnrxla@gmail.com>
2021-07-01 23:12:18 +0900

    Delete : Fix error

--- config/settings/prod.py
+++
@@ -1,7 +0,0 @@
-from .base import *
-
-ALLOWED_HOSTS = ['15.165.183.229']
-
-STATIC_ROOT = BASE_DIR / 'static/'
-
-STATICFILES_DIRS = []
--- rc_db/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-from .models import rc_db
-
-# Create your views here.
-def index(request):
-    return render(request, 'rc_db/index.html')
