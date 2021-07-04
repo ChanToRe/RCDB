eefbe0b332200420e207972247b99c6d2929a4b2
ChanHyeok <wnrnrxla@gmail.com>
2021-07-05 01:12:18 +0900

    Update : views.py
    Test Slack

--- rcdb/views.py
+++ rcdb/views.py
@@ -3,6 +3,5 @@ from django.http import HttpResponse
 from .models import rcdb
 from django.contrib.auth.models import User
 
-#Create your views here.
 def index(request):
     return render(request, 'rcdb/index.html')
