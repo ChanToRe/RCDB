3359658948110183344ec71583732455d472345b
ChanHyeok <wnrnrxla@gmail.com>
2021-07-08 22:20:17 +0900

    Update : urls.py
    json화

--- rcdb/urls.py
+++ rcdb/urls.py
@@ -4,5 +4,7 @@ from . import views
 app_name = 'rcdb'
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    #path('', views.index, name='index'),
+    path('get_RCDB/', views.get_RCDB, name='get_RCDB'),
+    path('apiRCDB/', views.apiRCDB, name='index')
 ]
