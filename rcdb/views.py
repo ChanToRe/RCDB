848db7085ecd1f03a8979561ef916cba84ca909f
ChanHyeok <wnrnrxla@gmail.com>
2021-07-08 22:20:24 +0900

    Update : views.py
    json화

--- rcdb/views.py
+++ rcdb/views.py
@@ -2,6 +2,14 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .models import rcdb
 from django.contrib.auth.models import User
+from django.core import serializers
+#def index(request):
+#   return render(request, 'rcdb/index.html')
 
-def index(request):
+def get_RCDB(request):
+    rcdbs = rcdb.objects.all()
+    rcdb_list = serializers.serialize('json', rcdbs)
+    return HttpResponse(rcdb_list, content_type="text/json-comment-filtered")
+
+def apiRCDB(request):
     return render(request, 'rcdb/index.html')
