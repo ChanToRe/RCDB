c07655d51f243e9514f15412c19cc380f1f442fa
Chan <64909586+ChanToRe@users.noreply.github.com>
2021-07-09 10:49:24 +0900

    Update : urls.py
    Handsontable 적용

--- rcdb/urls.py
+++ rcdb/urls.py
@@ -4,7 +4,5 @@ from . import views
 app_name = 'rcdb'
 
 urlpatterns = [
-    #path('', views.index, name='index'),
-    path('get_RCDB/', views.get_RCDB, name='get_RCDB'),
-    path('apiRCDB/', views.apiRCDB, name='index')
+    path('', views.index, name='index'),
 ]
